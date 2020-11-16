6a4774d5ee27a420dc66a488616826e9aec29691
Yurii Smal <smalygga1488@gmail.comgit config --global user.name Yurii>
2020-11-16 19:44:23 +0200

    router added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from '../Header/Header';
 import MainContainer from '../MainContainer/MainContainer';
 import Footer from '../Footer/Footer';
+import Maintenance from '../Maintenance/Maintenance'
 
 function App() {
   return (
     <div className="App">
-      <section>  <Header/></section>
+      {/* <section>  <Header/></section>
       <section> <MainContainer/></section>
-      <section> <Footer/></section>
+      <section> <Footer/></section> */}
+      <Maintenance/>
     </div>
   );
 }
--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import BooksContainer from '../../containers/BooksContainer';
 import './MainContainer.css'
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function MainContainer() {
   return (
-    <div className="Main-Container">
-      <section> <BooksContainer/></section>
+    <Router>
+    <div className="main-container">
+      <section className="main-container-wrapper">
+
+        <div className="main-card">
+          <Link to='/books'>BOOKS</Link>
+          <Switch>
+            <Route path='/books' component={BooksContainer}/>
+          </Switch>
+        </div>
+        
+        <div className="main-card">
+          <Link to='/movies'>MOVIES</Link>
+          <Switch>
+            <Route path='/movies' component={BooksContainer}/>
+          </Switch>
+        </div>
+       
+        <div className="main-card">
+          <Link to='/characters'>CHARACTERS</Link>
+          <Switch>
+            <Route path='/characters' component={BooksContainer}/>
+          </Switch>
+        </div>
+        
+        <div className="main-card">
+          <Link to='/quotes'>QUOTES</Link>
+          <Switch>
+            <Route path='/quotes' component={BooksContainer}/>
+          </Switch>
+        </div>
+       
+      </section>
     </div>
+    </Router>
   );
 }
 
