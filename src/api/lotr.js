76f185f8d3de44a002035b17f81afb0a974b2dc3
yakovyshynVitalii <vakovisin94@gmail.com>
2020-11-28 16:30:27 +0200

    delet project

--- src/api/lotr.js
+++
@@ -1,3 +0,0 @@
-export const getBooksInfo = () => fetch('https://the-one-api.dev/v2/book');
-export const getFilmsInfo = () => fetch('https://the-one-api.dev/v2/movie');
-export const getHeroesInfo = () => fetch('https://the-one-api.dev/v2/character');
